8072873578c2ee4562a947693a5a62873d1a4016
byrdm2000 <byrdm2000@gmail.com>
2020-01-31 19:29:35 -0500

    Remove unnecessary and non-functional lirc_handler.py

--- config.py
+++ config.py
@@ -1,4 +1,18 @@
-import lirc_handler
+import re
+
+
+def parse_config():
+    """
+    Parses the LIRC config file
+    :return: List, of all buttons specified in LIRC
+    """
+    buttons = []
+    with open(Config.LIRC_REMOTE_CONF_PATH) as lirc_conf:
+        for line in lirc_conf:
+            m = re.match('^\s*(\S*)\s*(?=0x00)', line)  # matches lines with 0x00 in them, puts non-whitespace in group
+            if m is not None:
+                buttons.append(m.group(1))
+    return buttons
 
 
 class Config:
@@ -15,4 +29,4 @@ class Config:
         'white': '#ffffff',
     }
 
-    COLORS = lirc_handler.parse_config()
+    COLORS = parse_config()
